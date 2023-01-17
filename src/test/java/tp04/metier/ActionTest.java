333eed7b122a6567b8600fc7471303679639b1b5
Alicia Perez <Alicia Perez@LAPTOP-NCR5NLJG>
2023-01-17 12:24:12 +0100

    commit number 2

---
+++ src/test/java/tp04/metier/ActionTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package tp04.metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Alicia Perez
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+    }
+    
+}
